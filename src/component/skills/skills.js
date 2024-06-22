0d2ca7fab957bed4e78f437b105cd003a6b3a16d
Asha <ashabarjo2021@gmail.com>
2024-06-22 14:34:00 +0530

    update-skills

--- src/component/skills/skills.js
+++ src/component/skills/skills.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './skills.css'
 const Skills = () => {
-  return (
-    <div className='skill container ps-5'>
+    return (
+        <div className='skill container ps-5'>
         <h1 className=' pt-5 title'>Skills</h1>
 
+        <div className="carousel">
         <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
  
         <div className="carousel-inner">
@@ -13,7 +14,7 @@ const Skills = () => {
 
     <div className=" row pt-5 ps-5">
         </div>
-            <div className="col-lg-3">
+            <div className="col-lg-3 col-md-3 col-sm-3">
                 <div className="skills-card card">
                     <h3>HTML</h3>
                     <img src="https://img.icons8.com/?size=100&id=20909&format=png&" alt="" />
@@ -22,7 +23,7 @@ const Skills = () => {
             </div>
 
             {/* 2 */}
-            <div className="col-lg-3">
+            <div className="col-lg-3 col-md-3 col-sm-3">
                 <div className="skills-card card">
                     <h3>CSS</h3>
 <img src="https://img.icons8.com/?size=100&id=7gdY5qNXaKC0&format=png&color=000000" alt="" />
@@ -31,7 +32,7 @@ const Skills = () => {
 
 
             {/* 3 */}
-            <div className="col-lg-3">
+            <div className="col-lg-3 col-md-3 col-sm-3">
                 <div className="skills-card card">
                     <h3>Javascript</h3>
 <img src="https://img.icons8.com/?size=100&id=tGvHBPJaKqEd&format=png&color=000000" alt="" />
@@ -39,7 +40,7 @@ const Skills = () => {
             </div>
 
 {/* 4 */}
-<div className="col-lg-3">
+<div className="col-lg-3 col-md-3 col-sm-3">
                 <div className="skills-card card">
                     <h3>React</h3>
 <img src="https://img.icons8.com/?size=100&id=wPohyHO_qO1a&format=png&color=000000" alt=""  />
@@ -52,7 +53,7 @@ const Skills = () => {
         <div className="row">
 {/* 2  row*/}
 <div className=" row pt-5 ps-5">
-            <div className="col-lg-3">
+            <div className="col-lg-3 col-md-3 col-sm-3">
                 <div className="skills-card card">
                     <h3>Bootstrap</h3>
                     <img src="https://img.icons8.com/?size=100&id=PndQWK6M1Hjo&format=png&color=000000" alt=""  />
@@ -61,7 +62,7 @@ const Skills = () => {
             </div>
 
             {/* 2 */}
-            <div className="col-lg-3">
+            <div className="col-lg-3 col-md-3 col-sm-3">
                 <div className="skills-card card">
                     <h3>Trailwind CSS</h3>
 <img src="https://img.icons8.com/?size=100&id=x7XMNGh2vdqA&format=png&color=000000" alt="" />
@@ -70,7 +71,7 @@ const Skills = () => {
 
 
             {/* 3 */}
-            <div className="col-lg-3">
+            <div className="col-lg-3 col-md-3 col-sm-3">
                 <div className="skills-card card">
                     <h3>CPP</h3>
 <img src="https://img.icons8.com/?size=100&id=40669&format=png&color=000000" alt="" />
@@ -78,7 +79,7 @@ const Skills = () => {
             </div>
 
 {/* 4 */}
-<div className="col-lg-3">
+<div className="col-lg-3 col-md-3 col-sm-3">
                 <div className="skills-card card">
                     <h3>Python</h3>
 <img src="https://img.icons8.com/?size=100&id=13441&format=png&color=000000" alt="" />
@@ -88,12 +89,13 @@ const Skills = () => {
 
         </div>
     </div>
+
     <div className="carousel-item">
         <div className="row">
     {/* 3rd row */}
     
     <div className=" row pt-5 ps-5">
-            <div className="col-lg-3">
+            <div className="col-lg-3 col-md-3 col-sm-3">
                 <div className="skills-card card">
                     <h3>Machine Learning</h3>
                     <img src="https://img.icons8.com/?size=100&id=gTN9eaZkKLFI&format=png&color=000000" alt="" />
@@ -102,7 +104,7 @@ const Skills = () => {
             </div>
 
             {/* 2 */}
-            <div className="col-lg-3">
+            <div className="col-lg-3 col-md-3 col-sm-3">
                 <div className="skills-card card">
                     <h3>PHP</h3>
 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwDJhSqTGNDdevfolscq5WA_nNItAro0_cfg&s" alt="" />
@@ -111,7 +113,7 @@ const Skills = () => {
 
 
             {/* 3 */}
-            <div className="col-lg-3">
+            <div className="col-lg-3 col-md-3 col-sm-3">
                 <div className="skills-card card">
                     <h3>Computer Networking</h3>
 <img src="https://img.icons8.com/?size=100&id=1HQrvpSeuu37&format=png&color=000000" alt=""/>
@@ -119,7 +121,7 @@ const Skills = () => {
             </div>
 
 {/* 4 */}
-<div className="col-lg-3">
+<div className="col-lg-3 col-md-4 col-sm-4">
                 <div className="skills-card card">
                     <h3>MS Office</h3>
 
@@ -174,7 +176,6 @@ const Skills = () => {
         </div>
 
     </div>
-  </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
     <span className="visually-hidden">Previous</span>
@@ -183,7 +184,10 @@ const Skills = () => {
     <span className="carousel-control-next-icon" aria-hidden="true"></span>
     <span className="visually-hidden">Next</span>
   </button>
+  </div>
+  </div>
 </div>
+
     </div>
   )
 }
