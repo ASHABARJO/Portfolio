a4a1fcd12e292c1ac5756d09fc9a8964b28b46d4
Asha <ashabarjo2021@gmail.com>
2024-06-29 15:05:39 +0530

    skill-update

--- src/component/skills/skills.js
+++ src/component/skills/skills.js
@@ -5,188 +5,77 @@ const Skills = () => {
         <div className='skill container ps-5'>
         <h1 className=' pt-5 title'>Skills</h1>
 
-        <div className="carousel">
-        <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
- 
-        <div className="carousel-inner">
-    <div className="carousel-item active">
-        <div className="row">
 
-    <div className=" row pt-5 ps-5">
-        </div>
-            <div className="col-lg-3 col-md-3 col-sm-3">
-                <div className="skills-card card">
-                    <h3>HTML</h3>
-                    <img src="https://img.icons8.com/?size=100&id=20909&format=png&" alt="" />
-                    
-                </div>
+        <div class="container">
+        <div class="gallery-row pt-5">
+            <div class="skills-card card">
+                <h3>HTML</h3>
+                <img src="https://img.icons8.com/?size=100&id=20909&format=png&" alt="" />
             </div>
-
-            {/* 2 */}
-            <div className="col-lg-3 col-md-3 col-sm-3">
-                <div className="skills-card card">
-                    <h3>CSS</h3>
-<img src="https://img.icons8.com/?size=100&id=7gdY5qNXaKC0&format=png&color=000000" alt="" />
-                </div>
+            <div class="skills-card card">
+                <h3>CSS</h3>
+                <img src="https://img.icons8.com/?size=100&id=7gdY5qNXaKC0&format=png&color=000000" alt="" />
             </div>
-
-
-            {/* 3 */}
-            <div className="col-lg-3 col-md-3 col-sm-3">
-                <div className="skills-card card">
-                    <h3>Javascript</h3>
-<img src="https://img.icons8.com/?size=100&id=tGvHBPJaKqEd&format=png&color=000000" alt="" />
-                </div>
+            <div class="skills-card card">
+                <h3>Javascript</h3>
+                <img src="https://img.icons8.com/?size=100&id=tGvHBPJaKqEd&format=png&color=000000" alt="" />
             </div>
-
-{/* 4 */}
-<div className="col-lg-3 col-md-3 col-sm-3">
-                <div className="skills-card card">
-                    <h3>React</h3>
-<img src="https://img.icons8.com/?size=100&id=wPohyHO_qO1a&format=png&color=000000" alt=""  />
-                </div>
+            <div class="skills-card card">
+                <h3>React</h3>
+                <img src="https://img.icons8.com/?size=100&id=wPohyHO_qO1a&format=png&color=000000" alt="" />
             </div>
-        </div>
-
-    </div>
-    <div className="carousel-item">
-        <div className="row">
-{/* 2  row*/}
-<div className=" row pt-5 ps-5">
-            <div className="col-lg-3 col-md-3 col-sm-3">
-                <div className="skills-card card">
-                    <h3>Bootstrap</h3>
-                    <img src="https://img.icons8.com/?size=100&id=PndQWK6M1Hjo&format=png&color=000000" alt=""  />
-                    
-                </div>
+            <div class="skills-card card">
+                <h3>Bootstrap</h3>
+                <img src="https://img.icons8.com/?size=100&id=PndQWK6M1Hjo&format=png&color=000000" alt="" />
             </div>
-
-            {/* 2 */}
-            <div className="col-lg-3 col-md-3 col-sm-3">
-                <div className="skills-card card">
-                    <h3>Trailwind CSS</h3>
-<img src="https://img.icons8.com/?size=100&id=x7XMNGh2vdqA&format=png&color=000000" alt="" />
-                </div>
+            <div class="skills-card card">
+                <h3>Tailwind CSS</h3>
+                <img src="https://img.icons8.com/?size=100&id=x7XMNGh2vdqA&format=png&color=000000" alt="" />
             </div>
-
-
-            {/* 3 */}
-            <div className="col-lg-3 col-md-3 col-sm-3">
-                <div className="skills-card card">
-                    <h3>CPP</h3>
-<img src="https://img.icons8.com/?size=100&id=40669&format=png&color=000000" alt="" />
-                </div>
+            <div class="skills-card card">
+                <h3>CPP</h3>
+                <img src="https://img.icons8.com/?size=100&id=40669&format=png&color=000000" alt="" />
             </div>
-
-{/* 4 */}
-<div className="col-lg-3 col-md-3 col-sm-3">
-                <div className="skills-card card">
-                    <h3>Python</h3>
-<img src="https://img.icons8.com/?size=100&id=13441&format=png&color=000000" alt="" />
-                </div>
+            <div class="skills-card card">
+                <h3>Python</h3>
+                <img src="https://img.icons8.com/?size=100&id=13441&format=png&color=000000" alt="" />
             </div>
-        </div>
-
+            <div class="skills-card card">
+                <h3>Machine Learning</h3>
+                <img src="https://img.icons8.com/?size=100&id=gTN9eaZkKLFI&format=png&color=000000" alt="" />
+            </div>
+            <div class="skills-card card">
+                <h3>PHP</h3>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwDJhSqTGNDdevfolscq5WA_nNItAro0_cfg&s" alt="" />
+            </div>
+            <div class="skills-card card">
+                <h3>Computer Networking</h3>
+                <img src="https://img.icons8.com/?size=100&id=1HQrvpSeuu37&format=png&color=000000" alt=""/>
+            </div>
+            <div class="skills-card card">
+                <h3>MS Office</h3>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfdjt09ToylrlABio3hpHFZbXpY1EtCM0qlQ&s" className='image-fluid' alt="" />
+            </div>
+            <div class="skills-card card">
+                <h3>AI content creator</h3>
+                <img src="https://media.licdn.com/dms/image/D4D12AQELjV14015icw/article-cover_image-shrink_720_1280/0/1698143188244?e=2147483647&v=beta&t=ADHnO1zBiIV_WZGVcJykORPD03mCr9XufDU8TL4tjKs" alt="" />
+            </div>
+            <div class="skills-card card">
+                <h3>DBMS</h3>
+                <img src="https://img.icons8.com/?size=100&id=11963&format=png&color=000000" alt="" />
+            </div>
+            <div class="skills-card card">
+                <h3>SQL</h3>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/8/87/Sql_data_base_with_logo.png" alt="" />
+            </div>
+            <div class="skills-card card">
+                <h3>Canva designer</h3>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqvc9rVVbJBlxO6W4Z5JlnSB_xhHdBE4iu4g&s" alt="" />
+            </div>
         </div>
     </div>
 
-    <div className="carousel-item">
-        <div className="row">
-    {/* 3rd row */}
-    
-    <div className=" row pt-5 ps-5">
-            <div className="col-lg-3 col-md-3 col-sm-3">
-                <div className="skills-card card">
-                    <h3>Machine Learning</h3>
-                    <img src="https://img.icons8.com/?size=100&id=gTN9eaZkKLFI&format=png&color=000000" alt="" />
-                    
-                </div>
-            </div>
 
-            {/* 2 */}
-            <div className="col-lg-3 col-md-3 col-sm-3">
-                <div className="skills-card card">
-                    <h3>PHP</h3>
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwDJhSqTGNDdevfolscq5WA_nNItAro0_cfg&s" alt="" />
-                </div>
-            </div>
-
-
-            {/* 3 */}
-            <div className="col-lg-3 col-md-3 col-sm-3">
-                <div className="skills-card card">
-                    <h3>Computer Networking</h3>
-<img src="https://img.icons8.com/?size=100&id=1HQrvpSeuu37&format=png&color=000000" alt=""/>
-                </div>
-            </div>
-
-{/* 4 */}
-<div className="col-lg-3 col-md-4 col-sm-4">
-                <div className="skills-card card">
-                    <h3>MS Office</h3>
-
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfdjt09ToylrlABio3hpHFZbXpY1EtCM0qlQ&s" className='image-fluid' alt="" />
-                </div>
-            </div>
-        </div>
-                
-        </div>
-      {/* <img src="..." class="d-block w-100" alt="..."> */}
-    </div>
-
-    <div className="carousel-item">
-        <div className="row">
-    {/* 3rd row */}
-    <div className=" row pt-5 ps-5">
-            <div className="col-lg-3">
-                <div className="skills-card card">
-                    <h3>AI content creator</h3>
-                    <img src="https://media.licdn.com/dms/image/D4D12AQELjV14015icw/article-cover_image-shrink_720_1280/0/1698143188244?e=2147483647&v=beta&t=ADHnO1zBiIV_WZGVcJykORPD03mCr9XufDU8TL4tjKs" alt="" />
-                    
-                </div>
-            </div>
-
-            {/* 2 */}
-            <div className="col-lg-3">
-                <div className="skills-card card">
-                    <h3>DBMS </h3>
-<img src="https://img.icons8.com/?size=100&id=11963&format=png&color=000000" alt="" />
-                </div>
-            </div>
-
-
-            {/* 3 */}
-            <div className="col-lg-3">
-                <div className="skills-card card">
-                    <h3>SQL</h3>
-<img src="https://upload.wikimedia.org/wikipedia/commons/8/87/Sql_data_base_with_logo.png" alt="" />
-                </div>
-            </div>
-            
-
-{/* 4 */}
-<div className="col-lg-3">
-                <div className="skills-card card">
-                    <h3>Canva designer</h3>
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqvc9rVVbJBlxO6W4Z5JlnSB_xhHdBE4iu4g&s" alt=""  />
-                </div>
-            </div>
-        </div>
-                
-        </div>
-
-    </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
-  </div>
-  </div>
-</div>
 
     </div>
   )
