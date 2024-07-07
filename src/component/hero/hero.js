23f9f2ca533289463ad109ead23d537e7f407f67
Asha <ashabarjo2021@gmail.com>
2024-07-08 00:54:59 +0530

    Update-hero

--- src/component/hero/hero.js
+++ src/component/hero/hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
         <div className="col-lg-7 col-md-12 text-container">
           <h1 className="title">Hello I'am</h1>
           <h3 className="subtitle">Web Developer and Machine Learning Enthusiast</h3>
-        <p>Front-end dev designs what you see, ML enthusiast explores AI's learning power.</p>
+        {/* <p>Front-end dev designs and ML enthusiast explores AI's learning power.</p> */}
         </div>
         <div className="col-lg-5 col-md-12">
           <img src="https://bit.ly/3VX265X" alt="Hero" className="hero-img ms-5" />
